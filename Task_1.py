4e216ba31e5839c5dfa1693bdaa9e164c6a8772c
Nihar360 <nikunaik2003@gmail.com>
2025-06-15 14:38:02 +0530

    Initial commit

---
+++ Task_1.py
@@ -0,0 +1,13 @@
+#Task 1: Perform Basic Mathematical Operations
+
+a = float(input("Enter no. 1 :"))
+b = float(input("Enter no. 2 :"))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+
+
+
---
+++ Task_2.py
@@ -0,0 +1,8 @@
+#Task 2: Create a Personalized Greeting
+
+first = input("Enter First Name: ")
+last = input("Enter Last Name: ")
+
+FullName = first + ' ' + last
+
+print("Hello," ,FullName + "! Welcome to the python program.")
